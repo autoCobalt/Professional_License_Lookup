4a421aae05972b210ddc3b60c973f6f8d2623508
Walter Alcazar <cobaltsignal@gmail.com>
2024-10-06 12:39:10 -0500

    folder src move

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from package_checker import check_and_install_packages
+from src.package_checker import check_and_install_packages
 check_and_install_packages({'requests', 'bs4', 'pandas', 'openpyxl'})
 
 
