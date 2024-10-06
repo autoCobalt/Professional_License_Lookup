08f3bcc5302ad06bebdfa7a9116e4ba957a59bce
Walter Alcazar <cobaltsignal@gmail.com>
2024-10-06 12:24:01 -0500

    testing check_and_install

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#to be added later
+from package_checker import check_and_install_packages
+check_and_install_packages({'requests', 'bs4', 'pandas', 'openpyxl'})
+
+
+def main():
+    print("this works!")
+
+
+if __name__ == '__main__':
+    main()
